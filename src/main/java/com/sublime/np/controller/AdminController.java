1bc4f04f775abae0cb0714ff617179edb82066a3
Iqbal sublime <Iqbal.sublime@gmail.com>
2015-05-18 01:49:28 +0600

    Controllers added

---
+++ src/main/java/com/sublime/np/controller/AdminController.java
@@ -0,0 +1,31 @@
+package com.sublime.np.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.sublime.np.service.UserService;
+
+@Controller
+public class AdminController {
+	
+	@Autowired
+	private UserService userService;
+	
+	@RequestMapping("/users")
+	public String users(Model model){
+		model.addAttribute("users", userService.findAll());
+		return "users";
+	}
+	
+	
+	
+	@RequestMapping("/user/remove/{id}")
+	public String removeUser(@PathVariable int id){
+		userService.delete(id);
+		return "redirect:/users.html";
+	}
+
+}
