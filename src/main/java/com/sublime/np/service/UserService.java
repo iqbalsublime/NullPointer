009e145e9bc5ab2defa1f3a252823aec20c9deec
Iqbal sublime <Iqbal.sublime@gmail.com>
2015-05-18 01:50:23 +0600

    Service classes added

---
+++ src/main/java/com/sublime/np/service/UserService.java
@@ -0,0 +1,57 @@
+package com.sublime.np.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.sublime.np.entity.Role;
+import com.sublime.np.entity.User;
+import com.sublime.np.repository.RoleRepository;
+import com.sublime.np.repository.UserRepository;
+
+@Service
+@Transactional
+public class UserService {
+	
+	@Autowired
+	private UserRepository userRepository;
+
+	
+	@Autowired
+	private RoleRepository roleRepository;
+	
+	public List<User> findAll(){
+		return userRepository.findAll();
+	}
+
+	public User findOne(int id) {
+		return userRepository.findOne(id);
+	}
+
+
+
+	public void save(User user) {
+		user.setEnabled(true);
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		user.setPassword(encoder.encode(user.getPassword()));
+		List<Role> roles = new ArrayList<Role>();
+		roles.add(roleRepository.findByName("ROLE_USER"));
+		user.setRoles(roles);
+		userRepository.save(user);
+	}
+
+
+	public void delete(int id) {
+		userRepository.delete(id);
+	}
+
+	public User findOne(String username) {
+		return userRepository.findByName(username);
+	}
+
+}
