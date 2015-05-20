da6e0e8da2b775047a4b5a61fa5550e6d687454b
Iqbal sublime <Iqbal.sublime@gmail.com>
2015-05-21 02:02:12 +0600

    Get all tag and get by id implemented

--- src/main/java/com/sublime/np/repository/TagRepository.java
+++ src/main/java/com/sublime/np/repository/TagRepository.java
@@ -2,11 +2,9 @@ package com.sublime.np.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.sublime.np.entity.Question;
 import com.sublime.np.entity.Tag;
 
 public interface TagRepository extends JpaRepository<Tag, Integer> {
-
-	
-
 
 }
--- src/main/java/com/sublime/np/service/TagService.java
+++ src/main/java/com/sublime/np/service/TagService.java
@@ -1,23 +1,37 @@
 package com.sublime.np.service;
 
+import java.util.List;
+
 import javax.transaction.Transactional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.sublime.np.entity.Question;
+import com.sublime.np.entity.Tag;
+import com.sublime.np.repository.QuestionRepository;
+import com.sublime.np.repository.TagRepository;
 
 @Service
 @Transactional
 public class TagService {
-/*	
+	
 	@Autowired
-	private UserRepository userRepository;
+	private TagRepository tagRepository;
 	
 	@Autowired
 	private QuestionRepository questionRepository;
 
-	@Autowired
-	private TagRepository tagRepository;
+	public List<Tag> findAll() {
+		return tagRepository.findAll();
+	}
 
+	public Tag findOne(int id) {
+		Tag tag = tagRepository.findOne(id);
+		List<Question> questions = questionRepository.findByTag(tag);
+		tag.setQuestions(questions);
+		return tag;
+	}
 
-	*/
 
 }
