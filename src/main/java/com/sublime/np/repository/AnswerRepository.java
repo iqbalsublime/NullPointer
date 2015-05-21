227a21208bdedc75af9ea8f37245c3d088d4d485
Iqbal sublime <Iqbal.sublime@gmail.com>
2015-05-22 02:37:12 +0600

    Get answers by user

--- src/main/java/com/sublime/np/repository/AnswerRepository.java
+++ src/main/java/com/sublime/np/repository/AnswerRepository.java
@@ -6,9 +6,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.sublime.np.entity.Answer;
 import com.sublime.np.entity.Question;
+import com.sublime.np.entity.User;
 
 public interface AnswerRepository extends JpaRepository<Answer, Integer> {
 
 	List<Answer> findByQuestion(Question question);
+
+	List<Answer> findByUser(User user);
 
 }
