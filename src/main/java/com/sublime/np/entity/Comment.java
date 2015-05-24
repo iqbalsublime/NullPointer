ceccaffc8de5d14a5404f8b69aee7e4e4f8a9ca6
Iqbal sublime <Iqbal.sublime@gmail.com>
2015-05-24 23:19:06 +0600

    Comment entity added

---
+++ src/main/java/com/sublime/np/entity/Comment.java
@@ -0,0 +1,78 @@
+package com.sublime.np.entity;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.Size;
+
+@Entity
+public class Comment {
+	
+	@Id
+	@GeneratedValue
+	private Integer id;
+
+	@Size(min=5, message = "Comment must be at least 5 characters !" )
+	private String commentText;
+	
+	@Column(name = "published_date")
+	private Date publishedDate;
+	
+	@ManyToOne
+	@JoinColumn(name="user_id")
+	private User user;
+	
+	@ManyToOne
+	@JoinColumn(name="question_id")
+	private Question question;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	
+
+
+	public String getCommentText() {
+		return commentText;
+	}
+
+	public void setCommentText(String commentText) {
+		this.commentText = commentText;
+	}
+
+	public Date getPublishedDate() {
+		return publishedDate;
+	}
+
+	public void setPublishedDate(Date publishedDate) {
+		this.publishedDate = publishedDate;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public Question getQuestion() {
+		return question;
+	}
+
+	public void setQuestion(Question question) {
+		this.question = question;
+	}
+
+	
+}
