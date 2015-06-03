fb02780f535050b56bcfa606c2f9212f5a9c4b5e
Iqbal sublime <Iqbal.sublime@gmail.com>
2015-06-04 00:27:56 +0600

    Object list as json implemented

--- src/main/java/com/sublime/np/controller/JSONController.java
+++ src/main/java/com/sublime/np/controller/JSONController.java
@@ -1,25 +1,68 @@
 package com.sublime.np.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.sublime.np.entity.Question;
 import com.sublime.np.entity.Shop;
+import com.sublime.np.entity.Tag;
+import com.sublime.np.entity.User;
+import com.sublime.np.service.QuestionService;
+import com.sublime.np.service.TagService;
 
 @Controller
 public class JSONController {
+	
+	
+	@Autowired
+	private QuestionService questionService;
 
-	@RequestMapping(value = "iqbal", method = RequestMethod.GET)
-	public @ResponseBody String getShopInJSON() {
+	@RequestMapping(value = "mkyoung", method = RequestMethod.GET)
+	public @ResponseBody Object getShopInJSON() {
 
 		Shop shop = new Shop();
 		shop.setName("G");
 		shop.setStaffName(new String[] { "mkyong1", "mkyong2" });
-		return shop.toString();
+		Shop shop1 = new Shop();
+		shop1.setName("G1");
+		shop1.setStaffName(new String[] { "mkyong1", "mkyong2" });
+		List<Shop> shops = new ArrayList<Shop>();
+		shops.add(shop1);
+		shops.add(shop);
+		
+		User user1 = new User();
+		user1.setId(1);
+		user1.setEmail("iqbal@gmail.com");
+		user1.setName("Iqbal");
+		User user2 = new User();
+		user2.setId(1);
+		user2.setEmail("iqbal@gmail.com");
+		user2.setName("Iqbal");
+		List<User> users = new ArrayList<User>();
+		users.add(user1);
+		users.add(user2);
+		
+		List<Question> questions = questionService.getAll();
+		
+		
+		return users;
 
 	}
+	
+//	@RequestMapping(value = "mkyoung", method = RequestMethod.GET)
+//	public @ResponseBody Shop getShopInJSON() {
+//
+//		Shop shop = new Shop();
+//		shop.setName("G");
+//		shop.setStaffName(new String[] { "mkyong1", "mkyong2" });
+//		return shop;
+//
+//	}
 
 }
